77df3e5c14e9ba664e0d29bb8dd7ea2e6cb71827
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-04-05 17:39:30 +0100

    some min fixes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,8 @@
 export function About () {
-    return <h1>About</h1>
+    return (
+        <>
+    <h1>About</h1>
+    <p>I have created this shopping cart app just to go deeper in my TypeScript learning path.</p>
+    </>
+    )
 }
